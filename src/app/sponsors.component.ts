a172c1344fcf930a3bdc325aab52b94e1857b152
Lloyd Cledwyn <cledwyn@gmail.com>
2016-12-18 21:19:16 -0600

    Change Sponsor message

--- src/app/sponsors.component.ts
+++ src/app/sponsors.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    template: `
-    
-  <iframe 
-    src="https://drive.google.com/file/d/0B1P_fRZsoCpXbXdZUVNENEFfQVZkUGtYN2ZqNWpSakhCVFh3/preview" 
-    style="width:100%;height:90vh;margin-top:57px;"></iframe>`,
+    templateUrl: 'sponsor.component.html',
 })
 export class SponsorsComponent  {
     constructor() { }

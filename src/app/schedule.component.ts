7e56b87e1db7f38dc909daeebbecd26f10758a4c
Lloyd Lentz <cledwyn@gmail.com>
2017-01-06 16:56:37 -0600

    Add track label icons

--- src/app/schedule.component.ts
+++ src/app/schedule.component.ts
@@ -19,24 +19,10 @@ export class ScheduleComponent {
     schedule;
     times;
 
-    tracks = [];
-
-    public checkButtonModel:any = {Keynote: false, Android: false, Web: false, Cloud: false, Design: false, IoT: false};
-
     constructor(public af: AngularFire) {
 
         this.schedule = af.database.list(PATH + '/schedule', { query: { orderByChild: 'title'} });
         this.times = af.database.list(PATH + "/scheduletimes");
     }
-    updateTrack(track){
-        console.log(track);
-    }
-
-    updatePie(pie){
-        console.log(pie);//this.favoritePie.push(pie);
-    }
-
-     buttonOpacity = 0.1;
-
 
 }

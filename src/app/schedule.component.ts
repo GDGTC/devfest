e76af4aa82c159b75f9145ebe03b5607290726d5
Lloyd Lentz <cledwyn@gmail.com>
2017-01-06 12:48:09 -0600

    mdHamFisting FTW

--- src/app/schedule.component.ts
+++ src/app/schedule.component.ts
@@ -5,6 +5,8 @@ import { FirebaseService, FirebaseTypedService } from './shared/firebase.service
 
 import { Observable } from 'rxjs/Observable';
 
+import {MdRipple } from '@angular/material/core/core';
+
 const PATH = 'devfest2017'
 
 @Component({
@@ -17,9 +19,24 @@ export class ScheduleComponent {
     schedule;
     times;
 
+    tracks = [];
+
+    public checkButtonModel:any = {Keynote: false, Android: false, Web: false, Cloud: false, Design: false, IoT: false};
+
     constructor(public af: AngularFire) {
+
         this.schedule = af.database.list(PATH + '/schedule', { query: { orderByChild: 'title'} });
         this.times = af.database.list(PATH + "/scheduletimes");
     }
+    updateTrack(track){
+        console.log(track);
+    }
+
+    updatePie(pie){
+        console.log(pie);//this.favoritePie.push(pie);
+    }
+
+     buttonOpacity = 0.1;
+
 
 }

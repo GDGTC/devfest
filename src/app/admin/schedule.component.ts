13337b6dd099ff02cf7a5f3424d983c75f2e7783
Lloyd Lentz <cledwyn@gmail.com>
2017-01-12 10:10:20 -0600

    working towards a Session Grid. Ideally with drag and drop placement

--- src/app/admin/schedule.component.ts
+++ src/app/admin/schedule.component.ts
@@ -14,6 +14,7 @@ const PATH = 'devfest2017'
     styleUrls: ['../shared/styles.css'],
     providers: [FirebaseService],
 })
+
 export class ScheduleComponent {
     isAdmin: Observable<boolean>;
     uid: Observable<string>;
@@ -21,6 +22,10 @@ export class ScheduleComponent {
 
     schedule;
     speakers;
+    scheduleTimes;
+    rooms; 
+
+    timeSlots;
 
     editSession:any = {};
     editSpeaker = {};
@@ -45,26 +50,66 @@ export class ScheduleComponent {
                 return null;
             }
         });
-        this.schedule = af.database.list(PATH + '/schedule',{ query: { orderByChild: 'startTime' } });
-
-        //  Atempt at mapping the Speakers to the speaker element in the Session.  ... abandoned
-//         this.schedule = af.database.list(PATH + '/schedule')
-// //            .map(items => items.sort((a, b) => a.startTime - b.startTime))
-//             .map(schedule => {
-//                 schedule.map(session=>{
-//                     session.speakers.map(speaker=> {
-//                         this.af.database.list(PATH + '/speakers')
-//                         .subscribe(sp => {
-//                             speaker = sp;
-//                         });
-//                     });
-//                     return session;
-//                 });
-//                 return schedule;
-//             }) as Observable<any[]>
-//         ;
-
+        this.schedule = af.database.list(PATH + '/schedule',{ query: { orderByChild: 'title' } });
         this.speakers = af.database.list(PATH + '/speakers', { query: { orderByChild: 'name' } });
+        this.scheduleTimes = af.database.list(PATH + '/scheduletimes',{query: {orderByChild: 'time'}});
+        this.rooms = af.database.list(PATH + '/rooms');
+
+
+         this.timeSlots = this.schedule
+            // Invert the array to be time-indexed.
+            .map(list => {
+                let times = {};
+                let rooms = {};
+                for (let session of list) {
+                    let index = session.startTime;
+                    let roomindex = session.room.replace("'","");
+                    //Make a collection of rooms
+                    if(Array.isArray(rooms[roomindex])){
+                        rooms[roomindex].push(session);
+                    } else {
+                        rooms[roomindex] = [session];
+                    }
+                    //Make a collection of times
+                    if(!Array.isArray(times[index])){
+                        times[index] = {};
+                    }
+                    if (Array.isArray(times[index][roomindex])) {
+                        times[index][roomindex].push(session);
+                    } else {
+                        times[index][roomindex] = [session];
+                    }
+
+                }
+//                delete times['UNK'];
+                let sortedSlots = Object.keys(times).sort();
+                let sortedRooms = Object.keys(rooms).sort();
+                console.log(times);
+                console.log(rooms);
+                let sessionGrid = {};
+                for (let slot of sortedSlots){
+                    sessionGrid[slot] = {};
+                    for (let room of sortedRooms){
+                        if(Array.isArray(times[slot][room])){
+                            sessionGrid[slot][room] = times[slot][room];
+                        }
+                        else {
+                            sessionGrid[slot][room] = [];
+                        }
+                    }
+                }
+                console.log(sessionGrid);
+                return { slots: sortedSlots, sessions: times, rooms: sortedRooms, sessiongrid: sessionGrid };
+            });
+
+        // Local Cache
+        this.timeSlots.subscribe(next => {
+            localStorage.setItem("scheduleCache", JSON.stringify(next));
+        });
+
+        this.timeSlots = this.timeSlots
+            .startWith(localStorage.getItem("scheduleCache"))
+            .shareResults();
 
     }
     login() {

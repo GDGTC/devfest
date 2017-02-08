5e1ce0e838f19d96854733e567025ef0c289b4c4
Stephen Fluin <stephen.fluin@gmail.com>
2017-02-08 13:50:08 -0800

    perf improvements

--- src/app/main/home.component.ts
+++ src/app/main/home.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 
+declare var google;
+
 @Component({
     templateUrl: './home.component.html'
 })
-export class HomeComponent  { }
+export class HomeComponent {
+    makeMapInteractive() {
+        new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 44.97418, lng:-93.279433 },
+            zoom: 16
+        });
+    }
+}

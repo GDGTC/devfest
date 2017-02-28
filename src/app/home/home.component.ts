05517f6ea13b7f8d0ff997353f27a662a4fa4638
Stephen Fluin <stephen.fluin@gmail.com>
2017-02-28 04:19:15 -0800

    performance optimizations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,4 @@ declare var google;
 @Component({
     templateUrl: './home.component.html'
 })
-export class HomeComponent {
-    makeMapInteractive() {
-        new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 44.97418, lng:-93.279433 },
-            zoom: 16
-        });
-    }
-}
+export class HomeComponent { }

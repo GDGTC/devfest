75249504e9626a97ea650d8d76a779084fef9234
Lloyd Lentz <cledwyn@gmail.com>
2019-01-16 17:59:03 -0600

    Minor tweaks

--- src/app/content/past.component.ts
+++ src/app/content/past.component.ts
@@ -6,10 +6,16 @@ import { Component } from '@angular/core';
             <div class="callout">Visit past DevFestMN content</div>
             <p>This is the 5th year of DevFestMN.</p>
             <mat-card style="width:400px;margin:16px 0;">
-                <mat-card-header><a href="/2017/speakers" style="font-size:24px;">2017 DevFestMN Speakers</a></mat-card-header>
+                <mat-card-header><a href="/2018/speakers" style="font-size:24px;">2018 Speakers</a></mat-card-header>
             </mat-card>
             <mat-card style="width:400px;margin:16px 0;">
-                <mat-card-header><a href="/2017/schedule" style="font-size:24px;">2017 DevFestMN Schedule</a></mat-card-header>
+                <mat-card-header><a href="/2018/schedule" style="font-size:24px;">2018 Schedule</a></mat-card-header>
+            </mat-card>
+            <mat-card style="width:400px;margin:16px 0;">
+                <mat-card-header><a href="/2017/speakers" style="font-size:24px;">2017 Speakers</a></mat-card-header>
+            </mat-card>
+            <mat-card style="width:400px;margin:16px 0;">
+                <mat-card-header><a href="/2017/schedule" style="font-size:24px;">2017 Schedule</a></mat-card-header>
             </mat-card>
             <mat-card style="width:400px;margin:16px 0;">
                 <mat-card-header><a href="/a/2016/" style="font-size:24px;">2016 DevFestMN Archive</a></mat-card-header>

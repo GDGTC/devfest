203e57fdb4933f6cc1e9ffbe77ec0420baeb8b26
Lloyd Lentz <cledwyn@gmail.com>
2019-08-09 10:02:29 -0500

    Add sectin to hide unconfirmed speakers

--- src/app/main/speaker-container.component.ts
+++ src/app/main/speaker-container.component.ts
@@ -3,11 +3,12 @@
  @Component({
      selector: 'speaker-container',
      template: `
-      <div class="speaker-card" *ngIf="speaker">
+      <div class="speaker-card" *ngIf="speaker.confirmed || showEdit">
         <div *ngIf="speaker.imageUrl">
             <div class="thumb" [style.background-image]="'url('+speaker.imageUrl+')'"></div>
         </div>
         <div class="speaker-content">
+            <div *ngIf="!speaker.confirmed && showEdit" style="background-color: yellow"><strong>NOT CONFIRMED</strong></div>
             <div style="font-size:20px;"><a [routerLink]="['/',year,'speakers',speaker.$key,speaker.name]">{{speaker.name}}</a>
             <a *ngIf="showEdit" [routerLink]="['/',year,'admin','speakers',speaker.$key,'edit']"><img src="/a/edit.svg"></a>
             </div>

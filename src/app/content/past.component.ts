a64c4ff09b172f3dcef68858dacf24df2061eacf
Lloyd Lentz <cledwyn@gmail.com>
2019-08-29 15:58:46 -0500

    Fix Past

--- src/app/content/past.component.ts
+++ src/app/content/past.component.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
             <div class="callout">Visit past DevFestMN content</div>
             <p>This is the 5th year of DevFestMN.</p>
             <mat-card style="width:400px;margin:16px 0;">
-                <mat-card-header><a href="/2017/speakers" style="font-size:24px;">2017 DevFestMN Speakers</a></mat-card-header>
+                <mat-card-header><a href="/2019/speakers" style="font-size:24px;">2019 DevFestMN Speakers</a></mat-card-header>
+            </mat-card>
+            <mat-card style="width:400px;margin:16px 0;">
+                <mat-card-header><a href="/2018/speakers" style="font-size:24px;">2018 DevFestMN Speakers</a></mat-card-header>
             </mat-card>
             <mat-card style="width:400px;margin:16px 0;">
                 <mat-card-header><a href="/2017/schedule" style="font-size:24px;">2017 DevFestMN Schedule</a></mat-card-header>
